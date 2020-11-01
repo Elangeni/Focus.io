4d5484f9a1e989515f57553c02208f9be8803366
Elangeni  Yabba <elangeniyabba@Elangenis-iMac.lan>
2020-10-31 23:53:47 -0400

    Todo list and timer are now functional. App is also now reponsive on mobile.

--- src/components/Pomodoro/Pomodoro.jsx
+++ src/components/Pomodoro/Pomodoro.jsx
@@ -1,88 +1,21 @@
-import React from 'react'
-import PomodoroTimer from './PomodoroTimer'
-import styles from './Pomodoro.module.css'
+import React from "react";
+import PomodoroTimer from "./PomodoroTimer";
+import styles from "./Pomodoro.module.css";
 
-export const Pomodoro = () => {
-  return (
-    <div className="">
-      <PomodoroTimer/>
-    </div>
-  )
+// export const Pomodoro = () => {
+//   return (
+//     <div className="">
+//       <PomodoroTimer/>
+//     </div>
+//   )
+// }
+
+class Pomodoro extends React.Component {
+  
+  render() {
+    return <PomodoroTimer 
+    />;
+  }
 }
 
 export default Pomodoro;
-
-
-// import React from "react";
-// import { Card, CardContent, Typography, Grid } from "@material-ui/core";
-// import styles from "./Pomodoro.module.css";
-
-// // Icons
-// import AddIcon from "@material-ui/icons/Add";
-// import RemoveIcon from "@material-ui/icons/Remove";
-
-// const Pomodoro = () => {
-//   var minutes = 25;
-//   var seconds = 0;
-//   var breakMinutes = 5;
-//   return (
-//     <div className={styles.container}>
-//       <div>
-//         <h1 className={styles.heading}>It's time to focus.</h1>
-//       </div>
-
-//       <div className={styles.gridContainer}>
-//         <div className={styles.gridItemHeading}>
-//           <p>Break length:</p>
-//         </div>
-//         <div className={styles.gridItemHeading}>
-//           <p>Session length:</p>
-//         </div>
-
-//         <div className={styles.gridItemLeft}>
-//           <button className={styles.icon}>
-//             <RemoveIcon />
-//           </button>
-//         </div>
-//         <div className={styles.gridItemMiddle}>
-//           <div class="col-sm timer-row" id="break-text">
-//             <p>{breakMinutes}:00</p>
-//           </div>
-//         </div>
-//         <div className={styles.gridItemRight}>
-//           <button className={styles.icon}>
-//             <AddIcon />
-//           </button>
-//         </div>
-
-//         <div className={styles.gridItemBlank}></div>
-//         <div className={styles.gridItemBlank}></div>
-
-//         <div className={styles.gridItemLeft}>
-//           <button className={styles.icon}>
-//             <RemoveIcon />
-//           </button>
-//         </div>
-//         <div className={styles.gridItemMiddle}>
-//           <div class="col-sm timer-row" id="session-text">
-//             <p>{minutes}:00</p>
-//           </div>
-//         </div>
-//         <div className={styles.gridItemRight}>
-//           <button className={styles.icon}>
-//             <AddIcon />
-//           </button>
-//         </div>
-//       </div>
-//       <div className={styles.timerContainer}>
-//         <h3>{minutes}:00</h3>
-//       </div>
-//       <div className={styles.btn}>
-//           <button className={styles.myButton}>Start</button>
-//           <button className={styles.myButton}>Reset</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Pomodoro;
--- src/components/Pomodoro/PomodoroTimer.jsx
+++ src/components/Pomodoro/PomodoroTimer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Pomodoro.module.css";
-import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 function PomodoroTimer() {
   var minutes = 25;
@@ -12,15 +12,15 @@ function PomodoroTimer() {
   var flag = false;
   var temp = true;
 
-  var minutes_interval = "";
-  var seconds_interval = "";
+  let minutes_interval = "";
+  let seconds_interval = "";
 
   // Initialize sounds
   var click = new Audio("click.mp3");
   var bell = new Audio("bell.mp3");
 
   const template = () => {
-      console.log("ok");
+    console.log("ok");
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = "00";
 
@@ -33,8 +33,101 @@ function PomodoroTimer() {
     seconds = 59;
   };
 
+  const minutesTimer = () => {
+    if (seconds === 60) {
+      minutes -= 1;
+    }
+    console.log(minutes);
+    document.getElementById("minutes").innerHTML = minutes;
+
+    if (minutes === 0 && seconds === 0) {
+      bell.play();
+    }
+  };
+
+  const secondsTimer = () => {
+    seconds -= 1;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (seconds < 10) {
+      document.getElementById("seconds").innerHTML = "0" + seconds.toString();
+    }
+
+    //Fix me
+    if (seconds <= 0) {
+      if (minutes === 0) {
+        clearInterval(minutes_interval);
+        clearInterval(seconds_interval);
+
+        bell.play();
+
+        document.getElementById("done").innerHTML =
+          "Session completed! Take a break.";
+        document.getElementById("done").classList.add("show_message");
+      }
+      seconds = 60;
+    }
+  };
+
+  //Change the session length
+  const increaseSession = () => {
+    //Check if timer has already started
+    if (flag) {
+      alert("Please stop and reset the timer to make any changes.");
+    }
+    if (minutes < 60) {
+      minutes += 1;
+      console.log(minutes);
+      document.getElementById("session-text").innerHTML = minutes;
+      document.getElementById("minutes").innerHTML = minutes;
+    } else {
+      alert("Session cannot be longer than 60 minutes.");
+    }
+  };
+
+  const decreaseSession = () => {
+    //Check if timer has already started
+    if (flag) {
+      alert("Please stop and reset the timer to make any changes.");
+    }
+    if (minutes > 5) {
+      minutes -= 1;
+      console.log(minutes);
+      document.getElementById("session-text").innerHTML = minutes;
+      document.getElementById("minutes").innerHTML = minutes;
+    } else {
+      alert("Session must be at least 5 minutes.");
+    }
+  };
+
+  //Change the break length
+  const increaseBreak = () => {
+    //Check if timer has already started
+    if (flag) {
+      alert("Please stop and reset the timer to make any changes.");
+    }
+    if (breakMinutes < 25) {
+      breakMinutes += 1;
+      document.getElementById("break-text").innerHTML = breakMinutes;
+    } else {
+      alert("Break cannot be longer than 25 minutes.");
+    }
+  };
+
+  const decreaseBreak = () => {
+    //Check if timer has already started
+    if (flag) {
+      alert("Please stop and reset the timer to make any changes.");
+    }
+    if (breakMinutes > 2) {
+      breakMinutes -= 1;
+      document.getElementById("break-text").innerHTML = breakMinutes;
+    } else {
+      alert("Break must be at least 2 minutes.");
+    }
+  };
+
   const start = () => {
-    console.log("lets begin");
     flag = true;
     click.play();
 
@@ -43,70 +136,70 @@ function PomodoroTimer() {
 
     minutes_interval = setInterval(minutesTimer, 1000);
     seconds_interval = setInterval(secondsTimer, 1000);
-
-    const minutesTimer = () => {
-      if (seconds === 60) {
-        minutes -= 1;
-      }
-      console.log(minutes);
-      document.getElementById("minutes").innerHTML = minutes;
-
-      if (minutes === 0 && seconds === 0) {
-        bell.play();
-      }
-    };
-
-    const secondsTimer = () => {
-      seconds -= 1;
-      document.getElementById("seconds").innerHTML = seconds;
-
-      if (seconds < 10) {
-        document.getElementById("seconds").innerHTML = "0" + seconds.toString();
-      }
-
-      //Fix me
-      if (seconds <= 0) {
-        if (minutes === 0) {
-          clearInterval(minutes_interval);
-          clearInterval(seconds_interval);
-
-          bell.play();
-
-          document.getElementById("done").innerHTML =
-            "Session completed! Take a break.";
-          document.getElementById("done").classList.add("show_message");
-        }
-        seconds = 60;
-      }
-    };
   };
+  
+
   return (
     <div className={styles.container} onLoad={template}>
       <h1>It's time to focus</h1>
-      <h4 id="minutes">{minutes}:00</h4>
+      <div className={styles.timer}>
+        <h4 id="minutes">{minutes}</h4>
+        <h4>:</h4>
+        <h4 id="seconds">{seconds === 0 ? "00" : seconds}</h4>
+      </div>
+
       <div>
-        <button className={styles.myButton} onClick={start}>
+        <div className={styles.buttons}>
+        <button
+          className={styles.myButton}
+          onClick={() => {
+            preStart();
+            start();
+          }}
+        >
           Start
         </button>
-        <button className={styles.myButton}>Reset</button>
+        <button
+          className={styles.myButton}
+          onClick={() => window.location.reload(false)}
+        >
+          Reset
+        </button>
+        </div>
+        
       </div>
       <div className={styles.makeChanges}>
-      <div className={styles.item}>
+        <div className={styles.item}>
+          
           <p>Set session length</p>
-          <AiOutlineMinus /> {sessionMinutes} <AiOutlinePlus/>
-      </div>
-      <div className={styles.item}>
+          <div className={styles.row}>
+          <button className={styles.changeButtons} onClick={decreaseSession}>
+            <AiOutlineMinus />
+          </button>
+          <p id="session-text">{sessionMinutes} </p>
+          <button className={styles.changeButtons} onClick={increaseSession}>
+            <AiOutlinePlus />
+          </button>
+          </div>
+
+          
+          
+        </div>
+        <div className={styles.item}>
           <p>Set break length</p>
-          <AiOutlineMinus /> {breakMinutes} <AiOutlinePlus/>
+          <div className={styles.row}>
+          <button className={styles.changeButtons} onClick={decreaseBreak}>
+            <AiOutlineMinus />
+          </button>
+          <p id="break-text">{breakMinutes}</p>
+          <button className={styles.changeButtons} onClick={increaseBreak}>
+            <AiOutlinePlus />
+          </button>
+          </div>
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 }
 
 export default PomodoroTimer;
-
-const Timer = () =>{
-    
-}
